45f74e5af3f9326ce251a9823ee751b071a59b82
Alena Kalivodová <kala13@vse.cz>
2021-12-19 13:44:55 +0100

    feat: added Location.java

--- src/main/java/en/fluffyBerserk/logic/objects/Entity.java
+++ src/main/java/en/fluffyBerserk/logic/objects/Entity.java
@@ -4,11 +4,17 @@ package en.fluffyBerserk.logic.objects;
  * Abstract class for all static objects
  */
 
-public abstract class Entity implements Object {
+public abstract class Entity implements Object, HasName {
+
+    private String name;
 
     private float x;
 
     private float y;
+
+    public Entity(){
+        name = getName();
+    }
 
     @Override
     public void setX(float x) {
