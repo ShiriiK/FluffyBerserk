ac20a789623d91226dc25e361a2734279c3ba9a6
Marek Mikula <marek.mikula01@gmail.com>
2022-01-11 23:21:44 +0100

    feat: adde comment

--- src/main/java/en/fluffyBerserk/game/Camera.java
+++ src/main/java/en/fluffyBerserk/game/Camera.java
@@ -11,6 +11,7 @@ public class Camera {
     public float processX(float x) {
         float offset = game.getPlayer().getX() - ((float)Constants.SCREEN_WIDTH / 2) + ((float)Constants.TILE_SIZE / 2);
 
+// This does not work yet, I don't know why, it looks cool but when the player comes to the bottom or left side, there is a slight bug in offset
 //        if (offset <= 0F) {
 //            offset = 0F;
 //        } else if (offset > (GameConstants.WORLD_WIDTH - GameConstants.SCREEN_WIDTH)) {
@@ -23,6 +24,7 @@ public class Camera {
     public float processY(float y) {
         float offset = game.getPlayer().getY() - ((float)Constants.SCREEN_HEIGHT / 2) + ((float)Constants.TILE_SIZE / 2);
 
+// This does not work yet, I don't know why, it looks cool but when the player comes to the bottom or left side, there is a slight bug in offset
 //        if (offset < 0F) {
 //            offset = 0F;
 //        } else if (offset > (GameConstants.WORLD_HEIGHT - GameConstants.SCREEN_HEIGHT)) {
